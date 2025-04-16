506a643eb987c41344a8f78856cd350f85d64d62
phiquangdat <datphi020603@gmail.com>
2025-04-16 19:00:55 +0300

    chapter-31

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import Greeting from "./components/Greeting";
+import "./App.css";
 
 function App() {
   return (
@@ -7,17 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more
-        </a>
-      </header>
+          Learn more{" "}
+        </a>{" "}
+      </header>{" "}
     </div>
   );
 }
---
+++ src/components/Greeting.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import { Output } from "./Output";
+const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+  return (
+    <div>
+      <h2> Hello World! </h2>{" "}
+      {!changedText && <Output> It 's good to see you!</Output>}{" "}
+      {changedText && <Output> Changed! </Output>}{" "}
+    </div>
+  );
+};
